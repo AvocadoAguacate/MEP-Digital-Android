656e716576f696e7bc3b61ab117c6ea09971209d
Esteban <estebanguzra@gmail.com>
2021-09-05 17:59:54 -0600

    Init_Teacher_UI

---
+++ app/src/main/java/com/example/mep_digital/Teacher/ChatTeacherActivity.java
@@ -0,0 +1,16 @@
+package com.example.mep_digital.Teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mep_digital.R;
+
+public class ChatTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_teacher);
+    }
+}
---
+++ app/src/main/java/com/example/mep_digital/Teacher/StudentsListTeacherActivity.java
@@ -0,0 +1,16 @@
+package com.example.mep_digital.Teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mep_digital.R;
+
+public class StudentsListTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_list_teacher);
+    }
+}
